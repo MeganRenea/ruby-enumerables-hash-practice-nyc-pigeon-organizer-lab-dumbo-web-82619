7cefe03c3f2edbd4cec12b0f735c2cdc3e5014b5
Megan Edmonds <meganr.edmonds@gmail.com>
2019-08-21 20:21:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,13 +5,13 @@ def nyc_pigeon_organizer(data)
       names.each do |name|
         if memo[name]
           if memo[name][key]
-          memo[name][key] << type
+          memo[name][key] << type.to_s
           binding.pry
           else
-          memo[name][key] = type
+          memo[name][key] = type.to_s
           end
         else
-          memo[name] = {key => [type]}
+          memo[name] = {key => [type.to_s]}
         end
       end
     end
