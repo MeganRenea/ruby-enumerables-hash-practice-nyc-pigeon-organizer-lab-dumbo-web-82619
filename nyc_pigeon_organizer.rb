003a63d7569a90cda23aae844f889234652e5621
Megan Edmonds <meganr.edmonds@gmail.com>
2019-08-21 20:23:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -18,27 +18,3 @@ def nyc_pigeon_organizer(data)
   end
   data
 end
-
-
-#require "pry"
-#def nyc_pigeon_organizer(data)
-  #data = data.reduce({}) do |memo, (key, values)|
-    #values.each do |item|
-      #names = item[1]
-      #names.each do |name|
-        #if memo[name]
-          #if memo[name][key]
-           # binding.pry
-          #memo[name][key] << item[0].to_s
-          #else
-          #memo[name][key] = [item[0].to_s]
-         # end
-        #else
-       #   memo[name] = {key => [item[0].to_s]}
-      #  end
-     # end
-    #end
-   # memo
-  #end
- # data
-#end
